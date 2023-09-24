76bc052eee7cd7c10529bebbd450d59ed39b3551
Specter43 <43552591+Specter43@users.noreply.github.com>
2023-09-24 15:55:21 -0400

    change division of 0 and related behaviors

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.example.calculator
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.TextView
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var numberField: TextView
+    private lateinit var numberField: EditText
     private var accumulatedValue: Double = 0.0
     private var currentOperation: String? = null
 
@@ -44,77 +43,101 @@ class MainActivity : AppCompatActivity() {
                 handleOperation(operation)
             }
         }
-        findViewById<Button>(R.id.sqrt).setOnClickListener {
-            calculateSqrt()
-        }
 
         findViewById<Button>(R.id.equal).setOnClickListener {
             calculateAndDisplayResult()
         }
 
         findViewById<Button>(R.id.clean).setOnClickListener {
-            numberField.text = "0"
-            accumulatedValue = 0.0
-            currentOperation = null
+            numberField.setText("0")
+            resetCalculatorState()
+        }
+
+        findViewById<Button>(R.id.sqrt).setOnClickListener {
+            calculateSqrt()
         }
     }
-
     private fun appendToInput(view: View) {
         val inputValue = (view as Button).text.toString()
-        if (numberField.text == formatResult(accumulatedValue)) {
-            numberField.text = ""
+        if (isErrorDisplayed() || numberField.text.toString() == formatResult(accumulatedValue)) {
+            numberField.setText("")
         }
-        numberField.text = "${numberField.text}$inputValue"
+        numberField.append(inputValue)
     }
 
     private fun handleOperation(operation: String) {
+        if (isErrorDisplayed()) return
+
         if (currentOperation != null) {
             calculateIntermediateResult()
         } else {
             accumulatedValue = numberField.text.toString().toDoubleOrNull() ?: 0.0
         }
-
         displayResult(accumulatedValue)
         currentOperation = operation
     }
 
     private fun calculateIntermediateResult() {
+        if (isErrorDisplayed()) return
+
         val inputValue = numberField.text.toString().toDoubleOrNull() ?: return
 
         when (currentOperation) {
             "+" -> accumulatedValue += inputValue
             "-" -> accumulatedValue -= inputValue
             "*" -> accumulatedValue *= inputValue
-            "/" -> if (inputValue != 0.0) {
-                accumulatedValue /= inputValue
-            } else {
-                numberField.text = "Error"
-                accumulatedValue = 0.0
-                currentOperation = null
-                return
+            "/" -> {
+                if (inputValue == 0.0) {
+                    displayError()
+                    return
+                } else {
+                    accumulatedValue /= inputValue
+                }
             }
         }
     }
 
+    private fun calculateAndDisplayResult() {
+        println("Calculate and Display")
+        calculateIntermediateResult()
+        if (!isErrorDisplayed()) {
+            displayResult(accumulatedValue)
+        }
+        currentOperation = null
+    }
+
     private fun calculateSqrt() {
+        println("SQRT")
         val currentValue = numberField.text.toString().toDoubleOrNull()
         if (currentValue != null && currentValue >= 0) {
             accumulatedValue = sqrt(currentValue)
             displayResult(accumulatedValue)
             currentOperation = null
         } else {
-            numberField.text = "Error"
+            numberField.setText("Error")
         }
     }
 
-    private fun calculateAndDisplayResult() {
-        calculateIntermediateResult()
-        displayResult(accumulatedValue)
+    private fun displayResult(value: Double) {
+        if (!isErrorDisplayed()) {
+            numberField.setText(formatResult(value))
+        } else {
+            resetCalculatorState()
+        }
+    }
+
+    private fun displayError() {
+        numberField.setText("Error")
+        resetCalculatorState()
+    }
+
+    private fun resetCalculatorState() {
+        accumulatedValue = 0.0
         currentOperation = null
     }
 
-    private fun displayResult(value: Double) {
-        numberField.text = formatResult(value)
+    private fun isErrorDisplayed(): Boolean {
+        return numberField.text.toString() == "Error"
     }
 
     private fun formatResult(value: Double): String {
